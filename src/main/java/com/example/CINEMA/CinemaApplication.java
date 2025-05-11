461c8ec06a9973b0b3c7bfc336c8273cd4e9702f
Karthik Grsk <karthikgrsk@gmail.com>
2025-05-11 18:17:50 +0530

    testing git

--- src/main/java/com/example/CINEMA/CinemaApplication.java
+++ src/main/java/com/example/CINEMA/CinemaApplication.java
@@ -9,5 +9,6 @@ public class CinemaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaApplication.class, args);
 	}
+	//TESTING GIT REPO BACKEND MOVIES
 //mongodb+srv://GOAT:s2DymOQ6P1Nj6Zov@cluster0.gewgg.mongodb.net/
 }
